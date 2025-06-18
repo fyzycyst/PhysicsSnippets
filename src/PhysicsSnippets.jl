30a54063d71fe2d5e83bb9f4ca0de0b2690a4b9d
fyzycyst <e.strobel.phd@gmail.com>
2025-06-18 14:17:05 -0500

    Add LinearAlgebra to Project.toml dependencies

--- src/PhysicsSnippets.jl
+++ src/PhysicsSnippets.jl
@@ -1,5 +1,16 @@
 module PhysicsSnippets
 
+using DifferentialEquations
+using CairoMakie
+using StaticArrays
+using Unitful
+using UnitfulAstro
+using LinearAlgebra
+using Test
+
+# Re-export commonly used functions
+export Planet, simulate_solar_system, plot_orbits_2d, calculate_energy
+
 # Export all the physics snippets
 include("01_PlanetaryOrbits/planetary_orbits.jl")
 # Add other includes as needed
